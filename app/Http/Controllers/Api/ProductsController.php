318b489eb4413def8c3de5d992ace174136a2a67
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-18 03:28:43 -0300

    aula 2

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+        $product = $user->products()->create($request->all());
+
+        return $product;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        if ($product->user_id != Auth::id()) {
+            abort(403);
+        }
+
+        $product->delete();
+
+        return response(null, 204);
+    }
+}
---
+++ tests/app/Http/Controllers/Api/ProductsControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ProductsControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testIndexReturnsAllProducts()
+    {
+        $products = factory(App\Product::class, 2)->create();
+
+        $this->get('api/products')->seeStatusCode(200);
+
+        foreach ($products as $product) {
+            $this->seeJson(['name' => $product->name]);
+        }
+    }
+
+    public function testStoreCreateAProduct()
+    {
+        $user = factory(App\User::class)->create();
+        $this->actingAs($user);
+
+        $this
+            ->post('api/products', [
+                'name' => 'Produto',
+                'price' => 10,
+            ])
+            ->seeStatusCode(200)
+            ->seeJson(['name' => 'Produto', 'price' => 10]);
+    }
+
+    public function testShowReturnsASpecificProduct()
+    {
+        $product = factory(App\Product::class)->create();
+
+        $this
+            ->get('api/products/' . $product->id)
+            ->seeStatusCode(200)
+            ->seeJson(['id' => $product->id, 'name' => $product->name]);
+    }
+
+    public function testAProductCanBeDeleted()
+    {
+        $product = factory(App\Product::class)->create();
+        $user = User::find(1);
+
+        $this
+            ->actingAs($user)
+            ->delete('api/products/1')
+            ->seeStatusCode(204)
+            ->isEmpty();
+
+        $this->notSeeInDatabase('products', ['id' => 1]);
+    }
+}
